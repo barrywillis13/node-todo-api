1fd460e6cdb6550f3d384095f2d1a3392b432745
Barry Willis <barrywillis13@gmail.com>
2016-12-05 15:35:47 +0000

    Added test cases for GET /todo/id

--- server/server.js
+++ server/server.js
@@ -51,7 +51,7 @@ app.get('/todos/:id', (req, res) => {
     if(todo){
       res.send({todo});
     }else {
-      return res.status(400).send();
+      return res.status(404).send();
     }
   }).catch((e) => console.log(e));
 });
